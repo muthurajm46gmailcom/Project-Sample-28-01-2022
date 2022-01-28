fbb86a7d8053b5b454ad37a3599afb54049a3069
muthurajm46gmailcom <muthurajm46@gmail.com>
2022-01-29 04:25:05 +0530

    ram completed work

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,6 +2,12 @@ package org.sample;
 
 public class Sample {
 	
+	private void m2() {
+		
+		System.out.println("ram completed work");
+
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
