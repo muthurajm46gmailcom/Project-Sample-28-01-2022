aba8ddfc56640daf55d2f67cb8a6b512ae9b76d5
muthurajm46gmailcom <muthurajm46@gmail.com>
2022-01-29 04:08:01 +0530

    muthu completed work

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,6 +2,10 @@ package org.sample;
 
 public class Sample {
 	
+	private void m1() {
+		System.out.println("my code");
+
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
