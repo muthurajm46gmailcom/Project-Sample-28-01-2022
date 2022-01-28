1f8feebb6520038233c68dc83ebad62e933e3e49
muthurajm46gmailcom <muthurajm46@gmail.com>
2022-01-28 23:41:11 +0530

    Sample Project Jan

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,13 @@
+package org.sample;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		System.out.println("hi");
+		System.out.println("bye");
+		
+	}
+
+}
